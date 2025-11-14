caa5a8873a4612172b96044cf1fe3d53ecf25e97
Antonia <a.dewoelmont@gmail.com>
2025-11-14 15:45:23 +0100

    fix: changed megaphone in a more cpp way

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,26 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ade-woel <ade-woel@student.s19.be>         +#+  +:+       +#+        */
+/*   By: ade-woel <ade-woel@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 16:21:43 by ade-woel          #+#    #+#             */
-/*   Updated: 2025/10/27 17:59:20 by ade-woel         ###   ########.fr       */
+/*   Updated: 2025/11/14 11:13:26 by ade-woel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-void	to_upper(char *str)
-{
-	int	i = -1;
-
-	while (str[++i])
-	{
-		if (str[i] >= 97 && str[i] <= 122)
-			str[i] -= 32;
-		std::cout << str[i];
-	}
-}
 
 int	main(int argc, char **argv)
 {
@@ -30,8 +18,11 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for (int i = 1; i < argc; i++)
-			to_upper(argv[i]);
+		for (int i = 1; i < argc; i++) 
+		{
+			for (int j = 0; argv[i][j] != '\0'; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+		}
 		std::cout << std::endl;
 	}
 	return (0);	
