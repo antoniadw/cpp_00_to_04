ad1a7412b60dd6a843236d917ffd9515be5e1edf
Antonia <a.dewoelmont@gmail.com>
2025-11-26 22:09:04 +0100

    fix: cpp01 ex04 file stream on Linux

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ade-woel <ade-woel@student.42belgium.be    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 21:43:23 by ade-woel          #+#    #+#             */
-/*   Updated: 2025/11/26 11:36:53 by ade-woel         ###   ########.fr       */
+/*   Updated: 2025/11/26 22:08:00 by ade-woel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,21 +34,21 @@ int	main(int argc, char **argv)
 		std::cerr << "Error, expecting 3 arguments [filename] [string1] [string2]" << std::endl;
 		return (1);
 	}
-	std::string		fileName = argv[1];
+	//std::string		fileName = argv[1];
 	std::string		s1 = argv[2];
 	if (s1.empty ()) {
 		std::cerr << "Error, s1 can not be empty" << std::endl;
 		return (1);
 	}
 	std::string		s2 = argv[3];
-	std::string		newFile = fileName + ".replace";
 	
-	std::ifstream	inputFile(fileName);
+	std::ifstream	inputFile(argv[1]);
 	if (!inputFile.is_open()) {
-		std::cerr << "Could not open input file " << fileName << std::endl;
+		std::cerr << "Could not open input file " << argv[1] << std::endl;
 		return (1);
 	}
-	std::ofstream	outputFile(newFile);
+	std::string		newFile = std::string(argv[1]) + ".replace";
+	std::ofstream	outputFile(newFile.c_str());
 	if (!outputFile.is_open()) {
 		std::cerr << "Could not open output file " << newFile << std::endl;
 		inputFile.close();
@@ -60,8 +60,8 @@ int	main(int argc, char **argv)
 	{
 		std::string	newLine = replaceString(line, s1, s2);
 		outputFile << newLine;
-		// if (inputFile.peek() != EOF)
-		// 	outputFile << '\n';
+		if (inputFile.peek() != EOF)
+			outputFile << '\n';
 	}
 	inputFile.close();
 	outputFile.close();
