47664fb32a3072698560b3e072d34a824f51ef2a
Antonia De Woelmont <ade-woel@mi-r8-p3.s19.be>
2025-11-27 15:36:00 +0100

    cleaned cpp01 ex03

--- cpp01/ex03/Weapon.cpp
+++ cpp01/ex03/Weapon.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Weapon.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ade-woel <ade-woel@student.42belgium.be    +#+  +:+       +#+        */
+/*   By: ade-woel <ade-woel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 18:55:53 by ade-woel          #+#    #+#             */
-/*   Updated: 2025/11/26 11:05:34 by ade-woel         ###   ########.fr       */
+/*   Updated: 2025/11/27 15:33:16 by ade-woel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ Weapon::Weapon(const char *type) {
 	if (type)
 		_type = std::string(type);
 	else
-		_type = "";
+		_type = "default";
 }
 
 Weapon::~Weapon() {}
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ade-woel <ade-woel@student.42belgium.be    +#+  +:+       +#+        */
+/*   By: ade-woel <ade-woel@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 18:55:46 by ade-woel          #+#    #+#             */
-/*   Updated: 2025/11/26 11:05:09 by ade-woel         ###   ########.fr       */
+/*   Updated: 2025/11/27 15:35:21 by ade-woel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,20 @@
 int	main ()
 {
 	{
-		Weapon	club = Weapon(NULL);
+		Weapon	club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
 		bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
 	}
 	{
-		Weapon	club = Weapon("crude spike club");
+		Weapon	club = Weapon("crude spiked club");
 		
 		HumanB	jim("Jim");
 		jim.setWeapon(club);
 		jim.attack();
 		club.setType("some other type of club");
 		jim.attack();
-	}
-	{
-		
 	}
 	return (0);
 }
@@ -42,3 +39,4 @@ int	main ()
 /* 1 - Replace Weapon param with NULL */
 /* 2 - Replace param with "" */
 /* 3 - Replace param with anything you like */
+/* 4 - Comment jim.setWeapon */
